e18f47eff8c0c5bce85d0c466f5b8fbbf5cc3ea8
Nikoloz750 <46031481+nika124@users.noreply.github.com>
2026-06-17 11:34:16 +0400

    Add initial models, repository contracts, and storage infrastructure

---
+++ BankingApplication/Interfaces/Infrastructure/JsonStorageProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApplication.Interfaces.Infrastructure
+{
+    internal class JsonStorageProvider
+    {
+    }
+}
---
+++ BankingApplication/Interfaces/Repositories/ICardRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApplication.Interfaces.Repositories
+{
+    internal interface ICardRepository
+    {
+    }
+}
